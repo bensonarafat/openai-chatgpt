94284ef2a794c90d5f4df962c3f917b35b70477d
Travis James <travis@tribehealthsolutions.com>
2023-01-13 03:25:11 -0600

    made the text animator stop animating after the first full render

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -2,7 +2,7 @@
 const endpoint = 'https://api.openai.com/v1/';
 
 /// API key
-const aiToken = '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';
+const aiToken = '[put token here]';
 
 /// Email for Gravatar image
-const emailAddress = 'travis@tribehealthsolutions.com';
+const emailAddress = '[insert email for gravatar]';
