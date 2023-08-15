455bce1259c89a80225cc2d47de5117dd3d6a097
Matheus Vinicius <matheusvinicius13ass@gmail.com>
2023-08-15 11:45:12 -0300

    visual and dart code changes and fix

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -2,7 +2,7 @@
 const endpoint = 'https://api.openai.com/v1/';
 
 /// API key
-const aiToken = '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';
+const aiToken = 'OPENIA API KEY';
 
 /// Email for Gravatar image
-const emailAddress = 'matheusvinicius13ass@gmail.com';
+const emailAddress = 'YOUR EMAIL';
